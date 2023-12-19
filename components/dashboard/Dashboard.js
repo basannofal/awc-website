cb502d82dce46389581b551b21cccdefb69e19d9
Ahmad <ahmadpadarwala4@gmail.com>
2023-12-19 13:36:30 +0500

    new

--- components/dashboard/Dashboard.js
+++ components/dashboard/Dashboard.js
@@ -20,50 +20,51 @@ const Dashboard = () => {
         <div className="dashboard-cards">
           <div className="dashboard-card card1">
             <div className="card-header">
-              <i className="fa-brands fa-product-hunt fa-beat-fade"></i>
+              <p>Product Category</p>
             </div>
             <div className="card-body">
+              <i className="fa-brands fa-product-hunt"></i>
               <h3>Product Cate...</h3>
               <p>$1,234,567</p>
             </div>
             <div className="card-footer">
-                <Link href="/admin/products-category">View Deatil</Link>
+              <Link href="/admin/products-category">View Detail...</Link>
             </div>
           </div>
           <div className="dashboard-card card2">
             <div className="card-header">
-              <i className="fa-brands fa-product-hunt fa-beat-fade"></i>
+              <i className="fa-brands fa-product-hunt"></i>
             </div>
             <div className="card-body">
               <h3>Products</h3>
               <p>3</p>
             </div>
             <div className="card-footer">
-                <Link href="/admin/admindashboard">View Deatil</Link>
+              <Link href="/admin/admindashboard">View Detail</Link>
             </div>
           </div>
           <div className="dashboard-card card3">
             <div className="card-header">
-              <i className="fa-solid fa-blog fa-beat-fade"></i>
+              <i className="fa-solid fa-blog"></i>
             </div>
             <div className="card-body">
               <h3>Blog Categories</h3>
               <p>1,234</p>
             </div>
             <div className="card-footer">
-                <Link href="/admin/blog-category">View Deatil</Link>
+              <Link href="/admin/blog-category">View Detail</Link>
             </div>
           </div>
           <div className="dashboard-card card4">
             <div className="card-header">
-              <i className="fa-solid fa-blog fa-beat-fade"></i>
+              <i className="fa-solid fa-blog"></i>
             </div>
             <div className="card-body">
               <h3>Blogs</h3>
               <p>567</p>
             </div>
             <div className="card-footer">
-                <Link href="/admin/blogs">View Deatil</Link>
+              <Link href="/admin/blogs">View Detail</Link>
             </div>
           </div>
         </div>
