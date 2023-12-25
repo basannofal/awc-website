4c0be0042abcb005768027042f4bb0bde6affc15
Ahmad <ahmadpadarwala4@gmail.com>
2023-12-25 13:47:04 +0500

    carrer page

--- components/client/Career/index.js
+++ components/client/Career/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import Navbar from '@/layouts/Client/Navbar';
-import HeroSection from './HeroSection';
-import Footer from '@/layouts/Client/Footer';
+import React from "react";
+import Navbar from "@/layouts/Client/Navbar";
+import HeroSection from "./HeroSection";
+import Form from "./Form";
+import Footer from "@/layouts/Client/Footer";
 
 const index = () => {
-    return (
-        <>
-            <Navbar />
-            <HeroSection />
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <Form />
+      <Footer />
+    </>
+  );
+};
 
-export default index
+export default index;
--- components/client/blogView/Blog.js
+++ components/client/blogView/Blog.js
@@ -172,163 +172,56 @@ const Blog = () => {
           <p className="related_heading">
             <span>Re</span>lated Articles
           </p>
-          <div className="mt-7">
-            <div className="main_related_blog_sec">
-              <div className="main_blog">
-                <div className="blog_image_sec">
-                  <img src={"/assets/images/client/blog-1.png"} alt="" />
-                  <img
-                    src={"/assets/images/client/client-img-1.png"}
-                    className="blog_category_icon"
-                    alt=""
-                  />
+        </div>
+      </section>
+      <section>
+        <div className="containers">
+          <div className="main_blogs">
+            <div className="flex flex-wrap justify-center">
+              <div className="sm:w-1/2 md:w-1/3 lg:w-3/12 blog_image_section">
+                <img src={"/assets/images/client/blog-1.png"} alt="" />
+                <div className="blog_name_main_section">
+                  <div className="blog_name_section">AWC India</div>
+                  <div className="blog_rectangle"></div>
+                  <div className="blog_name_section">2023-12-15</div>
                 </div>
-                <div className="blog_data_sec">
-                  <div className="blog_date_sec">
-                    <p className="name">John Smith</p>
-                    <p className="reading">
-                      <div></div>
-                      10 Min Read
-                    </p>
-                  </div>
-                  <p className="blog_heading">
-                    Wall Waterproofing Made Simple: Tips and Techniques
-                  </p>
-                  <p className="blog_desc">
-                    Learn the art of protecting your walls from moisture and
-                    cracks with our easy-to-follow guide.
-                  </p>
-                </div>
+                <p className="blog_desc_section">
+                  Unlocking the Secrets of Effective Roof Waterproofing
+                </p>
+                <p className="blog_sec_desc_section">
+                  Discover the key strategies and expert insights for achieving
+                  long-lasting roof waterproofing solutions.
+                </p>
               </div>
-              <div className="main_blog">
-                <div className="blog_image_sec">
-                  <img src={"/assets/images/client/blog-1.png"} alt="" />
-                  <img
-                    src={"/assets/images/client/client-img-1.png"}
-                    className="blog_category_icon"
-                    alt=""
-                  />
+              <div className="sm:w-1/2 md:w-1/3 lg:w-3/12 blog_image_section">
+                <img src={"/assets/images/client/blog-1.png"} alt="" />
+                <div className="blog_name_main_section">
+                  <div className="blog_name_section">AWC India</div>
+                  <div className="blog_rectangle"></div>
+                  <div className="blog_name_section">2023-12-15</div>
                 </div>
-                <div className="blog_data_sec">
-                  <div className="blog_date_sec">
-                    <p className="name">John Smith</p>
-                    <p className="reading">
-                      <div></div>
-                      10 Min Read
-                    </p>
-                  </div>
-                  <p className="blog_heading">
-                    Wall Waterproofing Made Simple: Tips and Techniques
-                  </p>
-                  <p className="blog_desc">
-                    Learn the art of protecting your walls from moisture and
-                    cracks with our easy-to-follow guide.
-                  </p>
-                </div>
+                <p className="blog_desc_section">
+                  Unlocking the Secrets of Effective Roof Waterproofing
+                </p>
+                <p className="blog_sec_desc_section">
+                  Discover the key strategies and expert insights for achieving
+                  long-lasting roof waterproofing solutions.
+                </p>
               </div>
-              <div className="main_blog">
-                <div className="blog_image_sec">
-                  <img src={"/assets/images/client/blog-1.png"} alt="" />
-                  <img
-                    src={"/assets/images/client/client-img-1.png"}
-                    className="blog_category_icon"
-                    alt=""
-                  />
+              <div className="sm:w-1/2 md:w-1/3 lg:w-3/12 blog_image_section">
+                <img src={"/assets/images/client/blog-1.png"} alt="" />
+                <div className="blog_name_main_section">
+                  <div className="blog_name_section">AWC India</div>
+                  <div className="blog_rectangle"></div>
+                  <div className="blog_name_section">2023-12-15</div>
                 </div>
-                <div className="blog_data_sec">
-                  <div className="blog_date_sec">
-                    <p className="name">John Smith</p>
-                    <p className="reading">
-                      <div></div>
-                      10 Min Read
-                    </p>
-                  </div>
-                  <p className="blog_heading">
-                    Wall Waterproofing Made Simple: Tips and Techniques
-                  </p>
-                  <p className="blog_desc">
-                    Learn the art of protecting your walls from moisture and
-                    cracks with our easy-to-follow guide.
-                  </p>
-                </div>
-              </div>
-              <div className="main_blog">
-                <div className="blog_image_sec">
-                  <img src={"/assets/images/client/blog-1.png"} alt="" />
-                  <img
-                    src={"/assets/images/client/client-img-1.png"}
-                    className="blog_category_icon"
-                    alt=""
-                  />
-                </div>
-                <div className="blog_data_sec">
-                  <div className="blog_date_sec">
-                    <p className="name">John Smith</p>
-                    <p className="reading">
-                      <div></div>
-                      10 Min Read
-                    </p>
-                  </div>
-                  <p className="blog_heading">
-                    Wall Waterproofing Made Simple: Tips and Techniques
-                  </p>
-                  <p className="blog_desc">
-                    Learn the art of protecting your walls from moisture and
-                    cracks with our easy-to-follow guide.
-                  </p>
-                </div>
-              </div>
-              <div className="main_blog">
-                <div className="blog_image_sec">
-                  <img src={"/assets/images/client/blog-1.png"} alt="" />
-                  <img
-                    src={"/assets/images/client/client-img-1.png"}
-                    className="blog_category_icon"
-                    alt=""
-                  />
-                </div>
-                <div className="blog_data_sec">
-                  <div className="blog_date_sec">
-                    <p className="name">John Smith</p>
-                    <p className="reading">
-                      <div></div>
-                      10 Min Read
-                    </p>
-                  </div>
-                  <p className="blog_heading">
-                    Wall Waterproofing Made Simple: Tips and Techniques
-                  </p>
-                  <p className="blog_desc">
-                    Learn the art of protecting your walls from moisture and
-                    cracks with our easy-to-follow guide.
-                  </p>
-                </div>
-              </div>
-              <div className="main_blog">
-                <div className="blog_image_sec">
-                  <img src={"/assets/images/client/blog-1.png"} alt="" />
-                  <img
-                    src={"/assets/images/client/client-img-1.png"}
-                    className="blog_category_icon"
-                    alt=""
-                  />
-                </div>
-                <div className="blog_data_sec">
-                  <div className="blog_date_sec">
-                    <p className="name">John Smith</p>
-                    <p className="reading">
-                      <div></div>
-                      10 Min Read
-                    </p>
-                  </div>
-                  <p className="blog_heading">
-                    Wall Waterproofing Made Simple: Tips and Techniques
-                  </p>
-                  <p className="blog_desc">
-                    Learn the art of protecting your walls from moisture and
-                    cracks with our easy-to-follow guide.
-                  </p>
-                </div>
+                <p className="blog_desc_section">
+                  Unlocking the Secrets of Effective Roof Waterproofing
+                </p>
+                <p className="blog_sec_desc_section">
+                  Discover the key strategies and expert insights for achieving
+                  long-lasting roof waterproofing solutions.
+                </p>
               </div>
             </div>
           </div>
