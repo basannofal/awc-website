5cb196a1bd867e7b49ab02eb106a9e0487154b16
Ahmad <ahmadpadarwala4@gmail.com>
2023-12-27 10:43:02 +0500

    blog view $ career page

--- components/client/blogView/Blog.js
+++ components/client/blogView/Blog.js
@@ -3,177 +3,188 @@ import React from "react";
 const Blog = () => {
   return (
     <>
-      <section className="blog-view-sec">
-        <div className="container">
-          <p className="blog-view-main-title">
-            blogs <i className="fa-solid fa-angles-right"></i>
-            <i className="fa-solid fa-angles-right"></i> wall section{" "}
-            <i className="fa-solid fa-angles-right"></i>
-            <i className="fa-solid fa-angles-right"></i> external wall
-            waterproofing - reason for failure & best solution
-          </p>
+      <section className="testi_hero_main">
+        <div
+          className="containeres"
+          style={{
+            padding: "170px 0px 50px 0px",
+          }}
+        >
+          <div className="blog-view-sec">
+            <p className="blog-view-main-title">
+              blogs <i className="fa-solid fa-angles-right"></i>
+              <i className="fa-solid fa-angles-right"></i> wall section{" "}
+              <i className="fa-solid fa-angles-right"></i>
+              <i className="fa-solid fa-angles-right"></i> external wall
+              waterproofing - reason for failure & best solution
+            </p>
 
-          <div className="blog_view_image">
-            <img
-              src={"/assets/images/client/blogViewImg.png"}
-              alt="lbog_view_image"
-            />
-          </div>
+            <div className="blog_view_image">
+              <img
+                src={"/assets/images/client/blogViewImg.png"}
+                alt="lbog_view_image"
+              />
+            </div>
 
-          <div className="blog_view_content_sec">
-            <div className="blog_content">
-              <div className="blog_date_section">
-                <p>
-                  <i className="fa-solid fa-calendar-days"></i> january 2023
-                </p>
-                <p>
-                  <i className="fa-solid fa-clock"></i> 2:00 PM
-                </p>
+            <div className="blog_view_content_sec">
+              <div className="blog_content">
+                <div className="blog_date_section">
+                  <p>
+                    <i className="fa-solid fa-calendar-days"></i> january 2023
+                  </p>
+                  <p>
+                    <i className="fa-solid fa-clock"></i> 2:00 PM
+                  </p>
+                </div>
+                <div className="blog_title">
+                  <p className="heading">
+                    External Wall Waterproofing – Reasons for failure & best
+                    solutions
+                  </p>
+                  <p className="blog_desc">
+                    Here are the 5 top reasons why external waterproofing fails
+                    and the 5 top solutions to rectify them. 1. Poor workmanship
+                    This is one of the key reasons why external waterproofing
+                    fails in most cases. Unlike earlier, today most external
+                    walls are not load-bearing walls, but their fundamental
+                    purpose to protect interiors continues. Poor workmanship
+                    during the construction phase or any repairs later can leave
+                    a lot of cracks causing the wall to allow moisture inside
+                    the structure. This failure to contain the moisture makes
+                    the wall damp thus attracting fungus and algae thus creating
+                    a hygiene threat to the lives of the people staying inside.
+                    Growth of such algae and fungus also creates dark patches
+                    which create a very undesirable look on the inside as well
+                    on the outside. If the waterproofing of the wall is not done
+                    right, eventually the wall may even become weak thus
+                    threatening the stability of the structure itself! 2. Use of
+                    inferior-quality material Another top reason for the failure
+                    of external wall waterproofing is the use of inferior or
+                    inadequate quantities of waterproofing material during the
+                    construction or repairs of the external walls. By its
+                    nature, cement emits heat when it is set. This heat is
+                    emitted through micro fissures and cracks in the walls.
+                    However, if the right varieties of waterproofing chemicals
+                    are not used in the right quantities then these cracks
+                    remain and water starts seeping through them over time. 3.
+                    Continuous leakage from pipes Most external walls have a
+                    system of pipes fitted on them that carries the stormwater
+                    or even the wastewater from the various amenities provided
+                    to the occupants. If the joints in such pipes are not sealed
+                    correctly, this wastewater starts leaking on the walls.
+                    Eventually, they may even lead to the growth of some algae,
+                    fungus, or even small plants which creates cracks in the
+                    external wall waterproofing. Continuous exposure to such
+                    wastewater can make the plaster layer weak and allow water
+                    to seep in. 4. Unplastered wall surfaces Many times, the
+                    external walls are either left fully unplastered or
+                    plastered minimally. This may be due to aesthetic reasons or
+                    due to financial constraints. But in either of the cases, an
+                    unplastered or poorly plastered wall allows moisture to seep
+                    in. 5. Puncture from outside If a wall is punctured from the
+                    outside by way of driving a nail or even to accommodate a
+                    new fixture, it may lead to the creation of microscopic
+                    cracks that are not visible to the naked eye. Over time,
+                    water starts seeping in through these cracks thus causing
+                    the external waterproofing to fail. However, not much is
+                    lost if you discover that the external wall waterproofing
+                    has failed. There exist a variety of exterior wall
+                    waterproofing solutions which can cure the problem and
+                    provide the occupants with long-term peace of mind.
+                  </p>
+                </div>
               </div>
-              <div className="blog_title">
-                <p className="heading">
-                  External Wall Waterproofing – Reasons for failure & best
-                  solutions
-                </p>
-                <p className="blog_desc">
-                  Here are the 5 top reasons why external waterproofing fails
-                  and the 5 top solutions to rectify them. 1. Poor workmanship
-                  This is one of the key reasons why external waterproofing
-                  fails in most cases. Unlike earlier, today most external walls
-                  are not load-bearing walls, but their fundamental purpose to
-                  protect interiors continues. Poor workmanship during the
-                  construction phase or any repairs later can leave a lot of
-                  cracks causing the wall to allow moisture inside the
-                  structure. This failure to contain the moisture makes the wall
-                  damp thus attracting fungus and algae thus creating a hygiene
-                  threat to the lives of the people staying inside. Growth of
-                  such algae and fungus also creates dark patches which create a
-                  very undesirable look on the inside as well on the outside. If
-                  the waterproofing of the wall is not done right, eventually
-                  the wall may even become weak thus threatening the stability
-                  of the structure itself! 2. Use of inferior-quality material
-                  Another top reason for the failure of external wall
-                  waterproofing is the use of inferior or inadequate quantities
-                  of waterproofing material during the construction or repairs
-                  of the external walls. By its nature, cement emits heat when
-                  it is set. This heat is emitted through micro fissures and
-                  cracks in the walls. However, if the right varieties of
-                  waterproofing chemicals are not used in the right quantities
-                  then these cracks remain and water starts seeping through them
-                  over time. 3. Continuous leakage from pipes Most external
-                  walls have a system of pipes fitted on them that carries the
-                  stormwater or even the wastewater from the various amenities
-                  provided to the occupants. If the joints in such pipes are not
-                  sealed correctly, this wastewater starts leaking on the walls.
-                  Eventually, they may even lead to the growth of some algae,
-                  fungus, or even small plants which creates cracks in the
-                  external wall waterproofing. Continuous exposure to such
-                  wastewater can make the plaster layer weak and allow water to
-                  seep in. 4. Unplastered wall surfaces Many times, the external
-                  walls are either left fully unplastered or plastered
-                  minimally. This may be due to aesthetic reasons or due to
-                  financial constraints. But in either of the cases, an
-                  unplastered or poorly plastered wall allows moisture to seep
-                  in. 5. Puncture from outside If a wall is punctured from the
-                  outside by way of driving a nail or even to accommodate a new
-                  fixture, it may lead to the creation of microscopic cracks
-                  that are not visible to the naked eye. Over time, water starts
-                  seeping in through these cracks thus causing the external
-                  waterproofing to fail. However, not much is lost if you
-                  discover that the external wall waterproofing has failed.
-                  There exist a variety of exterior wall waterproofing solutions
-                  which can cure the problem and provide the occupants with
-                  long-term peace of mind.
-                </p>
+              <div className="side_section">
+                <div className="side_sec_card">
+                  <p className="card_heading mb-4">Related Articles</p>
+                  <div className="article">
+                    <div className="article_icon">
+                      <img
+                        src={"/assets/images/client/client-img-1.png"}
+                        alt=""
+                      />
+                    </div>
+                    <div className="article_data">
+                      <p className="heading mb-1">Article Post-1</p>
+                      <p className="desc">
+                        University of Michigan freshman & Author of A Tangled
+                        Web
+                      </p>
+                    </div>
+                  </div>
+                  <hr
+                    style={{
+                      marginBottom: "20px",
+                      height: "1.6px",
+                      backgroundColor: "gray",
+                    }}
+                  />
+                  <div className="article">
+                    <div className="article_icon">
+                      <img
+                        src={"/assets/images/client/client-img-1.png"}
+                        alt=""
+                      />
+                    </div>
+                    <div className="article_data">
+                      <p className="heading mb-1">Article Post-1</p>
+                      <p className="desc">
+                        University of Michigan freshman & Author of A Tangled
+                        Web
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="side_sec_card">
+                  <p className="card_heading mb-3">Share With Your Friend</p>
+                  <div className="share_main_section">
+                    <div className="icon_title">
+                      <i className="fa-brands fa-facebook-f"></i>
+                      <p>Share On Facebook</p>
+                    </div>
+                    <div className="arrow">
+                      <i className="fa-solid fa-arrow-right"></i>
+                    </div>
+                  </div>
+                  <div className="share_main_section">
+                    <div className="icon_title">
+                      <i className="fa-brands fa-x-twitter"></i>
+                      <p>Share On Twitter</p>
+                    </div>
+                    <div className="arrow">
+                      <i className="fa-solid fa-arrow-right"></i>
+                    </div>
+                  </div>
+                  <div className="share_main_section">
+                    <div className="icon_title">
+                      <i className="fa-brands fa-whatsapp"></i>
+                      <p>Share On Whatsapp</p>
+                    </div>
+                    <div className="arrow">
+                      <i className="fa-solid fa-arrow-right"></i>
+                    </div>
+                  </div>
+                  <div className="share_main_section">
+                    <div className="icon_title">
+                      <i className="fa-brands fa-linkedin-in"></i>
+                      <p>Share On Linkedin</p>
+                    </div>
+                    <div className="arrow">
+                      <i className="fa-solid fa-arrow-right"></i>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="side_section">
-              <div className="side_sec_card">
-                <p className="card_heading mb-4">Related Articles</p>
-                <div className="article">
-                  <div className="article_icon">
-                    <img
-                      src={"/assets/images/client/client-img-1.png"}
-                      alt=""
-                    />
-                  </div>
-                  <div className="article_data">
-                    <p className="heading mb-1">Article Post-1</p>
-                    <p className="desc">
-                      University of Michigan freshman & Author of A Tangled Web
-                    </p>
-                  </div>
-                </div>
-                <hr
-                  style={{
-                    marginBottom: "20px",
-                    height: "1.6px",
-                    backgroundColor: "gray",
-                  }}
-                />
-                <div className="article">
-                  <div className="article_icon">
-                    <img
-                      src={"/assets/images/client/client-img-1.png"}
-                      alt=""
-                    />
-                  </div>
-                  <div className="article_data">
-                    <p className="heading mb-1">Article Post-1</p>
-                    <p className="desc">
-                      University of Michigan freshman & Author of A Tangled Web
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="side_sec_card">
-                <p className="card_heading mb-3">Share With Your Friend</p>
-                <div className="share_main_section">
-                  <div className="icon_title">
-                    <i className="fa-brands fa-facebook-f"></i>
-                    <p>Share On Facebook</p>
-                  </div>
-                  <div className="arrow">
-                    <i className="fa-solid fa-arrow-right"></i>
-                  </div>
-                </div>
-                <div className="share_main_section">
-                  <div className="icon_title">
-                    <i className="fa-brands fa-x-twitter"></i>
-                    <p>Share On Twitter</p>
-                  </div>
-                  <div className="arrow">
-                    <i className="fa-solid fa-arrow-right"></i>
-                  </div>
-                </div>
-                <div className="share_main_section">
-                  <div className="icon_title">
-                    <i className="fa-brands fa-whatsapp"></i>
-                    <p>Share On Whatsapp</p>
-                  </div>
-                  <div className="arrow">
-                    <i className="fa-solid fa-arrow-right"></i>
-                  </div>
-                </div>
-                <div className="share_main_section">
-                  <div className="icon_title">
-                    <i className="fa-brands fa-linkedin-in"></i>
-                    <p>Share On Linkedin</p>
-                  </div>
-                  <div className="arrow">
-                    <i className="fa-solid fa-arrow-right"></i>
-                  </div>
-                </div>
-              </div>
+            <div className="container mt-5">
+              <p className="related_heading">
+                <span>Re</span>lated Articles
+              </p>
             </div>
           </div>
-        </div>
-        <div className="container mt-5">
-          <p className="related_heading">
-            <span>Re</span>lated Articles
-          </p>
         </div>
       </section>
+
       <section>
         <div className="containers">
           <div className="main_blogs">
