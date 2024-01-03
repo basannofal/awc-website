a8fd176da45f1da0a907172052ebdeec9144bf90
basannofal <nofalbasan@gmail.com>
2024-01-03 19:43:46 +0530

    db update

--- pages/api/dbconfig/conn.js
+++ pages/api/dbconfig/conn.js
@@ -1,12 +1,20 @@
 import mysql from "mysql2";
 
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   // port: 'YOUR_MYSQL_PORT', // Typically 3306
+//   user: "root",
+//   password: "",
+//   database: "awc_db",
+//   connectionLimit: 10,
+// });
+
 const pool = mysql.createPool({
-  host: "localhost",
+  host: "aufcart.com",
   // port: 'YOUR_MYSQL_PORT', // Typically 3306
-  user: "root",
-  password: "",
-  database: "awc_db",
-  connectionLimit: 10,
+  user: "valudaaa_awc_db",
+  password: "A*244-yi+2b4",
+  database: "valudaaa_awc_db",
 });
 const conn = pool.promise();
 
