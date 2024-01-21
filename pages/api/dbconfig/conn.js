03d0b2f5a2e1a7bf259463b317bc4a97714a39cc
Nofal Basan <105139341+basannofal@users.noreply.github.com>
2024-01-21 13:39:04 +0530

    Update conn.js

--- pages/api/dbconfig/conn.js
+++ pages/api/dbconfig/conn.js
@@ -1,19 +1,19 @@
 import mysql from "mysql2";
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "awc_db",
-  connectionLimit: 10,
-});
-
 // const pool = mysql.createPool({
-//   host: "aufcart.com",
-//   user: "valudaaa_awc_db",
-//   password: "A*244-yi+2b4",
-//   database: "valudaaa_awc_db",
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "awc_db",
+//   connectionLimit: 10,
 // });
+
+const pool = mysql.createPool({
+  host: "aufcart.com",
+  user: "valudaaa_awc_db",
+  password: "A*244-yi+2b4",
+  database: "valudaaa_awc_db",
+});
 
 
 // const pool = mysql.createPool({
