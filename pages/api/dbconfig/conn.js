a2b23598944c15a45c3320b8101fe129751a90cf
basannofal <nofalbasan@gmail.com>
2024-01-21 14:33:26 +0530

    DB updated

--- pages/api/dbconfig/conn.js
+++ pages/api/dbconfig/conn.js
@@ -8,20 +8,28 @@ import mysql from "mysql2";
 //   connectionLimit: 10,
 // });
 
-const pool = mysql.createPool({
-  host: "aufcart.com",
-  user: "valudaaa_awc_db",
-  password: "A*244-yi+2b4",
-  database: "valudaaa_awc_db",
-});
+// const pool = mysql.createPool({
+//   host: "178.16.139.144",
+//   user: "root",
+//   password: "Ansi@#2023",
+//   database: "awc",
+// });
 
 
 // const pool = mysql.createPool({
-//   host: "codinghelps.com",
-//   user: "codinghelps_awc",
-//   password: "KsXz7t1^n(@m",
-//   database: "codinghelps_awc",
+//   host: "aufcart.com",
+//   user: "valudaaa_awc_db",
+//   password: "A*244-yi+2b4",
+//   database: "valudaaa_awc_db",
 // });
+
+
+const pool = mysql.createPool({
+  host: "valudas.com",
+  user: "valudas_awc",
+  password: "yya94Z3.B9sl",
+  database: "valudas_awc",
+});
 
 const conn = pool.promise();
 
