2ffd0bdf768f1c89cf65a05357f386b88278dc29
Ahmad <ahmadpadarwala4@gmail.com>
2024-01-21 18:21:12 +0500

    changes final

--- pages/api/dbconfig/conn.js
+++ pages/api/dbconfig/conn.js
@@ -1,27 +1,12 @@
 import mysql from "mysql2";
 
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "awc_db",
-//   connectionLimit: 10,
-// });
-
 const pool = mysql.createPool({
-  host: "aufcart.com",
-  user: "valudaaa_awc_db",
-  password: "A*244-yi+2b4",
-  database: "valudaaa_awc_db",
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "awc_db",
+  connectionLimit: 10,
 });
-
-
-// const pool = mysql.createPool({
-//   host: "codinghelps.com",
-//   user: "codinghelps_awc",
-//   password: "KsXz7t1^n(@m",
-//   database: "codinghelps_awc",
-// });
 
 const conn = pool.promise();
 
