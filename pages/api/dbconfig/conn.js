f14a6faec43753b4b2c6d258f20cdac5bdfeaf87
basannofal <nofalbasan@gmail.com>
2024-01-22 13:30:29 +0530

    some changes

--- pages/api/dbconfig/conn.js
+++ pages/api/dbconfig/conn.js
@@ -1,12 +1,12 @@
 import mysql from "mysql2";
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "awc_db",
-  connectionLimit: 10,
-});
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "awc_db",
+//   connectionLimit: 10,
+// });
 
 
 // const pool = mysql.createPool({
@@ -25,12 +25,12 @@ const pool = mysql.createPool({
 // });
 
 
-// const pool = mysql.createPool({
-//   host: "valudas.com",
-//   user: "valudas_awc",
-//   password: "yya94Z3.B9sl",
-//   database: "valudas_awc",
-// });
+const pool = mysql.createPool({
+  host: "valudas.com",
+  user: "valudas_awc",
+  password: "yya94Z3.B9sl",
+  database: "valudas_awc",
+});
 
 const conn = pool.promise();
 
