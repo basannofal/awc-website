1187b39714f6c423c6389a735094a3bdc9ae1d42
Ahmad <ahmadpadarwala4@gmail.com>
2024-02-26 15:32:55 +0500

    product images height fix2

--- pages/api/dbconfig/conn.js
+++ pages/api/dbconfig/conn.js
@@ -1,18 +1,10 @@
 import mysql from "mysql2";
 
-// const pool = mysql.createPool({
-//   host: process.env.NEXT_PUBLIC_DB_HOST,
-//   user: process.env.NEXT_PUBLIC_DB_USER,
-//   password: process.env.NEXT_PUBLIC_DB_USER_PASS,
-//   database: process.env.NEXT_PUBLIC_DB_NAME,
-//   connectionLimit: 10,
-// });
-
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "awc_db",
+  host: process.env.NEXT_PUBLIC_DB_HOST,
+  user: process.env.NEXT_PUBLIC_DB_USER,
+  password: process.env.NEXT_PUBLIC_DB_USER_PASS,
+  database: process.env.NEXT_PUBLIC_DB_NAME,
   connectionLimit: 10,
 });
 
